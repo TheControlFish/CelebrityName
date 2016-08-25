b29b88d354977ef1e5c349fd0fcbac79967e556d
TheControlFish <jacef314@gmail.com>
2016-08-25 12:02:11 -0600

    Add files via upload

---
+++ Celebrity.java
@@ -0,0 +1,17 @@
+
+public class Celebrity {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1 = "Allan Alda";
+		String s2 = "John Wayne";
+		String s3 = "Gregory Peck";
+		String part1 = s1.substring(2, s1.length()-3);
+		String part2 = s2.substring(2, s2.length()-3);
+		String part3 = s3.substring(2, s3.length()-3);
+		System.out.println(part1);
+		System.out.println(part2);
+		System.out.println(part3);
+	}
+
+}
